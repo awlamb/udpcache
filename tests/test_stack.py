f4214b1c8578d3235370a8db492dcd7d44901148
Alexander Lamb <awill310@gmail.com>
2016-09-23 14:51:10 -0700

    add test to test stack pop

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -5,8 +5,12 @@ class StackTestCase(unittest.TestCase):
     def setUp(self):
         self.stack = Stack()
 
-    def test_push(self):
+    def test_add(self):
         assert self.stack.add('key1','value1') == True
+
+    def test_pop(self):
+        self.stack.add('key1','value1')
+        assert self.stack.pop('key1') == 'value1'
 
 if __name__ == '__main__':
     unittest.main()
